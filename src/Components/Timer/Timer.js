79810d11b217bbdb8d2bedb4bc06874589db1b6f
mehul bansal <87509823+mehul1409@users.noreply.github.com>
2023-12-28 12:29:48 +0530

    Update Timer.js

--- src/Components/Timer/Timer.js
+++ src/Components/Timer/Timer.js
@@ -2,14 +2,6 @@ import React ,{useState,useRef,useEffect} from 'react'
 import "./Timer.css"
 export default function Timer() {
 
-
-  useEffect(()=>{
-    startTimer();
-    return()=>{
-      clearInterval(interval.current);
-    }
-  })
-
   return (
     <div className='timer-container'>
        
