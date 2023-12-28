187e3a444c16191b4ea552f9bf76b52688684194
rishika-on-git <aggarwalrishika135@gmail.com>
2023-12-28 20:03:50 +0530

    added navbar layout

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,11 +3,46 @@ import { Link } from 'react-router-dom';
 import './Header.css';
 
 const Header = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
+  const closeMobileMenu = () => {
+    setIsMobileMenuOpen(false);
+  };
 
   return (
     <>
-    
+      <div className="navbar-fixed">
+        <div className="navBar">
+          <div className="hamburger" onClick={toggleMobileMenu}>
+            <div className="line"></div>
+            <div className="line"></div>
+            <div className="line"></div>
+          </div>
+          <div className="jscop" id="jscop">
+            JSCOP
+          </div>
+          <div
+            className={`navbar-items ${isMobileMenuOpen ? "mobilemenu" : ""}`}
+          >
+            <a onClick={closeMobileMenu}>
+            <Link to='/'>Home</Link>
+            </a>
+            <a onClick={closeMobileMenu}>
+            <Link to='/team'>Team</Link>
+            </a>
+            <a onClick={closeMobileMenu}>
+            <Link to='/gallery'>Gallery</Link>
+            </a>
+            <a onClick={closeMobileMenu}>
+            <Link to='/contact'>Contact us</Link>
+            </a>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
